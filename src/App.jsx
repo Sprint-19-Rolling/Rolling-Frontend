ed40fb23da1bf1b91449ce2c7245c0ec0f1dac01
Choi hee ruk <chlgmlfkr593@naver.com>
2025-09-26 19:05:39 +0900

    🐛 Fix: Router 누락으로 인한 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Router from '@/router/Router';
 import Header from './components/common/Header';
 
 const App = () => {
   return (
     <div>
       <Header />
-      {/* 나머지 앱 컨텐츠 */}
+      <Router />
     </div>
   );
 };
