8f22c21dec371dbb66ccea66260e0d6f2893834c
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-26 17:00:50 +0900

    ✨ Feat: 링크 버튼 컴포넌트 추가

---
+++ src/components/common/LinkButton.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router';
+import { buttonVariants } from '@/style/button-style';
+import { cn } from '@/utils/style';
+
+/**
+ * 공통 링크 버튼 컴포넌트 (페이지 이동을 위해 사용)
+ * @param props.children - 링크 버튼 내부에 표시될 내용
+ * @param props.to - 이동 경로
+ * @param props.theme - 링크 버튼 스타일 테마 ('primary' | 'secondary' | 'outlined' | 'icon') 기본값: primary
+ * @param props.size - 링크 버튼 높이 (28 | 32 | 36 | 40 | 56) 기본값: null
+ * @param props.full - 풀 사이즈 링크 버튼 여부 기본값: false
+ */
+
+const LinkButton = ({
+  to,
+  children,
+  theme = 'primary',
+  size = null,
+  full = false,
+}) => {
+  return (
+    <Link to={to} className={cn(buttonVariants({ theme, size, full }))}>
+      {children}
+    </Link>
+  );
+};
+
+export default LinkButton;
