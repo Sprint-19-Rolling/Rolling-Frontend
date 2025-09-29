8945cff906c7e27f1faf0e91ae00b3cae5e4c467
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-09-29 12:55:41 +0900

    ✨ Feat/dropdown기능구현

--- src/components/common/Dropdown.jsx
+++ src/components/common/Dropdown.jsx
@@ -16,6 +16,12 @@ export default function ProfileDropdown() {
       }
     };
 
+    const handleKeyDown = (event) => {
+      if(event.key === "Escape") {
+        setIsDropdownOpen(false);
+      }
+    }
+
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
@@ -24,14 +30,14 @@ export default function ProfileDropdown() {
     <div className="relative inline-block" ref={ref}>
       <button
         onClick={toggleDropdown}
-        className="flex items-center justify-center w-9 h-9 mr-32 bg-transparent border-none text-white"
+        className="flex items-center justify-center w-9 h-9 bg-transparent border-none text-white"
       >
         <AccountCircleIcon className="text-white" sx={{ fontSize: 30 }} />
       </button>
 
       {isDropdownOpen && (
         <div className="absolute top-10 right-0 bg-gray-800 text-white p-2 rounded shadow-lg z-50">
-          <ul className="flex flex-col list-none m-0 p-0">
+          <ul className="flex flex-col list-none m-0 p-0 gap-6">
             <li className="border border-gray-900 rounded-lg px-3 py-2 cursor-pointer hover:bg-gray-700">texttexttext</li>
             <li className="rounded-lg px-3 py-2 cursor-pointer hover:bg-gray-700">texttexttext</li>
             <li className="rounded-lg px-3 py-2 cursor-pointer hover:bg-gray-700">texttexttext</li>
