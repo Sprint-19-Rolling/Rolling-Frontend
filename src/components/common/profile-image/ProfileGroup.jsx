140c950c03c9f76fe15b32fe6e71f5acb636edcb
양은지 <eunji124@github.com>
2025-09-29 13:12:22 +0900

    ✨ Feat: 공통 컴포넌트 프로필 개별, 그룹, 선택 추가 - prop-types적용, key값 url로 변경, 폰트 테일윈드 적용

---
+++ src/components/common/profile-image/ProfileGroup.jsx
@@ -0,0 +1,40 @@
+// ProfileGroup.jsx
+// 프로필 그룹 컴포넌트
+import PropTypes from 'prop-types';
+import ProfileImage from './ProfileImage';
+import '@/style/style.css';
+
+const ProfileGroup = ({ participants }) => {
+  const visible = participants.slice(0, 3);
+  const hiddenCount = participants.length - visible.length;
+
+  return (
+    <div className="flex items-center">
+      {visible.map((url) => {
+        return (
+          <ProfileImage
+            key={url}
+            src={url}
+            size="small"
+            borderColor="white"
+            isSelected={false}
+            isClickable={false}
+            className="-ml-2"
+          />
+        );
+      })}
+
+      {hiddenCount > 0 && (
+        <div className="-ml-2 flex h-7 w-7 items-center justify-center rounded-full border-1 border-gray-300 bg-white font-12-medium text-gray-600">
+          +{hiddenCount}
+        </div>
+      )}
+    </div>
+  );
+};
+
+ProfileGroup.propTypes = {
+  participants: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default ProfileGroup;
