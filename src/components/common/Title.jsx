3df7ff7b4da7c095cfa43a6b6ff6f1caf4c0ad58
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-29 10:07:39 +0900

    ✨ Feat: Title 컴포넌트 as, className prop 추가

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types';
+import { cn } from '@/utils/style';
 
-const Title = ({ children }) => {
-  return <h2 className="font-24-bold text-gray-900">{children}</h2>;
+const Title = ({ children, className, as: Component = 'h2' }) => {
+  return (
+    <Component className={cn('font-24-bold text-gray-900', className)}>
+      {children}
+    </Component>
+  );
 };
 
 Title.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  as: PropTypes.oneOf(['h2', 'h3', 'h4', 'h5', 'h6']),
 };
 
 export default Title;
