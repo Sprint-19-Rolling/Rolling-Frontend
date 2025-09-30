a0081fe69dac533e0b5cc9ffcf3b618c879b88f3
Choi hee ruk <chlgmlfkr593@naver.com>
2025-09-30 10:53:09 +0900

    ✨ Feat: Post 페이지에 GenericInput 컴포넌트로 수신자 이름 입력 기능 구현

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import GenericInput from '@/components/common/GenericInput';
+
 const Post = () => {
-  return <div />;
+  const [toValue, setToValue] = useState('');
+
+  return (
+    <>
+      <div>
+        <GenericInput
+          label="To."
+          name="To"
+          placeholder="받는 사람 이름을 입력해 주세요"
+          errorMessage="이름을 입력해 주세요"
+          value={toValue}
+          onChange={setToValue}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Post;
