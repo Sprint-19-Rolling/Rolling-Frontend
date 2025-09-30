f40123a985b01ad2c4502bcaac4c3c9439674b64
Choi hee ruk <chlgmlfkr593@naver.com>
2025-09-30 11:35:13 +0900

    ✨ Feat: PostMessage 페이지에 GenericInput 컴포넌트로 발신자 이름 입력 기능 구현

--- src/pages/PostMessage.jsx
+++ src/pages/PostMessage.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import GenericInput from '@/components/common/GenericInput';
+
 const PostMessage = () => {
-  return <div />;
+  const [myValue, setMyValue] = useState('');
+
+  return (
+    <>
+      <div>
+        <GenericInput
+          label="From."
+          name="From"
+          placeholder="이름을 입력해 주세요"
+          errorMessage="이름을 입력해 주세요"
+          value={myValue}
+          onChange={setMyValue}
+        />
+      </div>
+    </>
+  );
 };
 
 export default PostMessage;
