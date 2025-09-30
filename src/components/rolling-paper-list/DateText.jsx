df21f0f8bc64496b20f9639f0a17997053b1f425
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-30 20:04:07 +0900

    ✨ Feat: size prop 추가

--- src/components/rolling-paper-list/DateText.jsx
+++ src/components/rolling-paper-list/DateText.jsx
@@ -1,9 +1,20 @@
+import { cva } from 'class-variance-authority';
 import PropTypes from 'prop-types';
 import { formatDate } from '@/utils/formatDate';
+import { cn } from '@/utils/style';
 
-const DateText = ({ createdAt }) => {
+const dataTextVariants = cva(`text-gray-400`, {
+  variants: {
+    size: {
+      card: 'font-12-regular',
+      modal: 'font-14-regular',
+    },
+  },
+});
+
+const DateText = ({ createdAt, size = 'card' }) => {
   return (
-    <time dateTime={createdAt} className="font-12-regular text-gray-400">
+    <time dateTime={createdAt} className={cn(dataTextVariants({ size }))}>
       {formatDate(createdAt)}
     </time>
   );
@@ -11,6 +22,7 @@ const DateText = ({ createdAt }) => {
 
 DateText.propTypes = {
   createdAt: PropTypes.string.isRequired,
+  size: PropTypes.string,
 };
 
 export default DateText;
