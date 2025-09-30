a2d99f4e0ffbb96844123a619319268c0a3427cd
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-30 16:46:47 +0900

    ♻️ Refactor: 날짜 포맷팅 방법 변경

--- src/hooks/formatDate.js
+++ src/hooks/formatDate.js
@@ -4,7 +4,9 @@ export const formatDate = (isoDate) => {
   }
 
   const date = new Date(isoDate);
-  const formatted = date.toISOString().split('T')[0].replace(/-/g, '.');
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
 
-  return formatted;
+  return `${year}.${month}.${day}`;
 };
