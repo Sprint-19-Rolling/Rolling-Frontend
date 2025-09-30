d491318609b58b12c1fd1ec91b953ddc98042cba
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-30 19:11:38 +0900

    ✨ Feat: prop 클래스 네임 추가, 반응형 디자인 추가

--- src/components/rolling-paper-list/AuthorInfo.jsx
+++ src/components/rolling-paper-list/AuthorInfo.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 import RelationshipBadge from '@/components/common/badge/RelationshipBadge';
 import ProfileImage from '@/components/common/profile-image/ProfileImage';
+import { cn } from '@/utils/style';
 
-const AuthorInfo = ({ sender, profileImageURL, relationship }) => {
+const AuthorInfo = ({ sender, profileImageURL, relationship, className }) => {
   return (
-    <div className="flex gap-3.5">
+    <div className={cn('flex gap-3.5', className)}>
       <ProfileImage
         src={profileImageURL}
         alt={sender + ' 프로필 이미지'}
         borderColor="gray"
       />
       <div>
-        <div className="font-20-regular mb-1.5 flex gap-1.5 leading-6">
+        <div className="font-16-regular sm:font-20-regular mb-0.5 flex gap-1.5 leading-6 text-gray-900 sm:mb-1.5">
           <span>From.</span>
           <span className="font-bold">{sender}</span>
         </div>
