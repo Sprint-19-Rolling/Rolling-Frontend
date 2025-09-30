9bfb40851ffe7275e78e09f233d24b39eaf33ac9
Choi hee ruk <chlgmlfkr593@naver.com>
2025-09-30 16:12:44 +0900

    🐛 Fix: UseState를 하나의 hook파일로 만들면서 수정 및 화살표 함수 변경

--- src/components/common/GenericInput.jsx
+++ src/components/common/GenericInput.jsx
@@ -1,47 +1,31 @@
-import { useState } from 'react';
 import TextInput from '@/components/common/TextInput';
 
-export default function GenericInput({
+const GenericInput = ({
   name,
   placeholder,
-  errorMessage,
+  label,
   value,
   onChange,
-  label,
-}) {
-  const [error, setError] = useState('');
-
+  onBlur,
+  error,
+  errorMessage,
+}) => {
   const finalPlaceholder =
     placeholder ?? (label ? `${label}을(를) 입력해 주세요` : '');
-  const finalErrorMessage =
-    errorMessage ?? (label ? `${label}을(를) 입력해 주세요` : '');
-
-  const validate = (v) => {
-    if (!v.trim()) {
-      setError(finalErrorMessage);
-      return false;
-    }
-    setError('');
-    return true;
-  };
 
   return (
-    <div className={`w-full`}>
+    <div className="w-full">
       <TextInput
         name={name}
         placeholder={finalPlaceholder}
         value={value}
-        onChange={(v) => {
-          onChange(v);
-
-          if (v.trim()) {
-            setError('');
-          }
-        }}
-        onBlur={(v) => validate(v)}
-        error={!!error}
-        errorMessage={error}
+        onChange={onChange}
+        onBlur={onBlur}
+        error={error}
+        errorMessage={errorMessage}
       />
     </div>
   );
-}
+};
+
+export default GenericInput;
