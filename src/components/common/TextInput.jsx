2eb06dcdf481f3799b76926197bd4ee9d33a260f
Choi hee ruk <chlgmlfkr593@naver.com>
2025-09-30 16:14:32 +0900

    🐛 Fix: cva사용으로 인한 내용 및 화살표 함수 수정

--- src/components/common/TextInput.jsx
+++ src/components/common/TextInput.jsx
@@ -1,26 +1,39 @@
+import { cva } from 'class-variance-authority';
 import { cn } from '@/utils/style';
 
-function TextInput({
+const inputWrapper = cva(
+  'flex w-full items-center gap-2.5 rounded-lg border-2 bg-white px-3 py-3 focus-within:outline-none active:outline-none',
+  {
+    variants: {
+      error: {
+        true: 'border-red-500 focus-within:border-red-500 active:border-red-500',
+        false:
+          'border-gray-300 focus-within:border-gray-500 active:border-gray-700',
+      },
+    },
+    defaultVariants: {
+      error: false,
+    },
+  }
+);
+
+const TextInput = ({
   name,
   type = 'text',
   placeholder,
   value,
   onChange,
   onBlur,
-  error,
+  error = false,
   errorMessage,
   className = '',
   disabled = false,
   ...props
-}) {
+}) => {
   return (
     <div className="flex w-full flex-col items-center gap-2">
       <div className="w-full">
-        <div
-          className={cn(
-            'flex w-full items-center gap-2.5 rounded-lg border-2 bg-white px-3 py-3 focus-within:border-[#555] active:border-[#3A3A3A]',
-            error ? 'border-red-500' : 'border-gray-300'
-          )}>
+        <div className={inputWrapper({ error })}>
           <input
             id={name}
             name={name}
@@ -30,7 +43,10 @@ function TextInput({
             onBlur={(e) => onBlur && onBlur(e.target.value)}
             placeholder={placeholder}
             disabled={disabled}
-            className={`flex-1 bg-transparent font-['Pretendard'] text-base font-normal leading-[26px] tracking-[-0.16px] text-[#181818] outline-none placeholder:text-gray-400 hover:text-[#555] disabled:cursor-not-allowed disabled:opacity-50 ${className} `}
+            className={cn(
+              "flex-1 bg-transparent font-['Pretendard'] text-base font-normal leading-[26px] tracking-[-0.16px] text-gray-900 outline-none placeholder:text-gray-400 hover:text-gray-600 disabled:cursor-not-allowed disabled:opacity-50",
+              className
+            )}
             {...props}
           />
         </div>
@@ -40,6 +56,6 @@ function TextInput({
       </div>
     </div>
   );
-}
+};
 
 export default TextInput;
