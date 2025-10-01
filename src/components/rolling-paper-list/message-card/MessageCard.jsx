947e33482f0a82beed4fa0db451e61936a905733
ahreum <117738875+aahreum@users.noreply.github.com>
2025-10-01 18:03:33 +0900

    ✨ Feat: 롤링페이퍼 메세지 카드 컴포넌트 구현

---
+++ src/components/rolling-paper-list/message-card/MessageCard.jsx
@@ -0,0 +1,60 @@
+import { cva } from 'class-variance-authority';
+import AuthorInfo from '@/components/rolling-paper-list/AuthorInfo';
+import DateText from '@/components/rolling-paper-list/DateText';
+import { cn } from '@/utils/style';
+
+const textStyle = cva(
+  'font-15-regular sm:font-18-regular line-clamp-2 text-gray-600 sm:line-clamp-3',
+  {
+    variants: {
+      font: {
+        Pretendard: 'font-sans',
+        'Noto Sans': 'ff-noto',
+        나눔명조: 'ff-nanum-myeongjo',
+        '나눔손글씨 손편지체': 'ff-nanum-sonpyeonji',
+      },
+    },
+  }
+);
+
+/**
+ * 롤링페이퍼 목록에서 사용되는 메시지 카드 컴포넌트입니다.
+ * 클릭 시 해당 메시지의 상세 모달을 띄웁니다.
+ * @param {object} props
+ * @param {string} props.sender - 메시지 작성자의 이름
+ * @param {string} props.profileImageURL - 작성자의 프로필 이미지 URL
+ * @param {string} props.relationship - 작성자와 받는 사람의 관계
+ * @param {string} props.createdAt - 메시지가 작성된 날짜
+ * @param {string} props.content - 메시지의 본문 내용
+ * @param {'Pretendard'|'Noto Sans'|'나눔명조'|'나눔손글씨 손편지체'} props.font - 메시지 내용에 적용할 폰트 변수
+ * @param {function} props.onClick - 카드를 클릭했을 때 실행될 핸들러 함수 (모달 열기)
+ * @returns {JSX.Element}
+ */
+
+const MessageCard = ({
+  sender,
+  profileImageURL,
+  relationship,
+  createdAt,
+  content,
+  font,
+  onClick,
+}) => {
+  return (
+    <button
+      type="button"
+      className="card-style cursor-pointer flex-col items-start gap-4 p-6 pt-7"
+      onClick={onClick}>
+      <AuthorInfo
+        sender={sender}
+        profileImageURL={profileImageURL}
+        relationship={relationship}
+        className={'pb-3.75 border-b border-gray-200'}
+      />
+      <div className={cn(textStyle({ font }))}>{content}</div>
+      <DateText className={'mt-auto'} createdAt={createdAt} />
+    </button>
+  );
+};
+
+export default MessageCard;
