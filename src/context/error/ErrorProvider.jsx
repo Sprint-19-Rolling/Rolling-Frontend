3d5d1517391bc5a79a71e1d949bc57233acb9f5c
ahreum <117738875+aahreum@users.noreply.github.com>
2025-10-02 20:28:47 +0900

    📝 Docs: JSDoc 타입 정의 수정 및 상세 설명 추가

--- src/context/error/ErrorProvider.jsx
+++ src/context/error/ErrorProvider.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import { ErrorContext } from './errorContext';
 
 /**
- * 전역적인 에러 상태를 관리하고 하위 컴포넌트에 제공하는 프로바이더 컴포넌트입니다.
- * - `error`: 현재 에러 메시지 데이터 (`null` 또는 `string`)
- * - `setError`: 에러 상태를 설정하는 React의 상태 함수
+ * 전역 에러 상태를 관리하고 하위 컴포넌트에 제공하는 프로바이더 컴포넌트입니다.
+ *
+ * 이 컴포넌트는 Context를 통해 다음 객체를 제공합니다:
+ * - `error`: 현재 에러 메시지 데이터 { status: number, message: string } 객체 혹은 `null`.
+ * - `setError`: 에러 상태를 설정하는 함수 (ErrorPayload 객체 또는 null)을 인수로 받음
+ * * @typedef {import('./errorContext').ErrorPayload} ErrorPayload - ErrorContext에 정의된 에러 객체 타입
+ *
  * @param {object} props - 컴포넌트의 props
- * @param {React.ReactNode} props.children - Context의 상태를 공유할 하위 React 요소들
- * @returns {JSX.Element} Context Provider 컴포넌트.
+ * @param {React.ReactNode} props.children - Context의 상태를 공유할 하위 React 요소
+ * @returns {JSX.Element} Context Provider 컴포넌트
  */
 
 const ErrorProvider = ({ children }) => {
--- src/context/error/errorContext.js
+++ src/context/error/errorContext.js
@@ -1,12 +1,19 @@
 import { createContext } from 'react';
 
 /**
- * 전역 에러 상태와 관리 함수를 제공하는 Context입니다.
- * @typedef {Object} ErrorContextType
- * @property {string | null} error - 현재 표시할 에러 메시지
- * @property {(message: string) => void} setError - 에러 메시지를 설정하는 함수
+ * @typedef {Object} ErrorPayload
+ * @property {number} status - HTTP 상태 코드 (예: 404, 500).
+ * @property {string} message - 사용자에게 표시할 에러 메시지.
  */
 
+/**
+ * 전역 에러 상태와 관리 함수를 제공하는 Context입니다.
+ *
+ * @typedef {Object} ErrorContextType
+ * @property {ErrorPayload | null} error - 현재 표시할 에러 객체. 에러가 없으면 null.
+ * @property {(errorData: ErrorPayload | null) => void} setError - 에러 상태를 설정하는 함수.
+ * (null을 전달하면 에러 상태 초기화)
+ */
 /**
  * @type {React.Context<ErrorContextType>}
  */
