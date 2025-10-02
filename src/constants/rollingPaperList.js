f5b3fbc0e30fed6dec08024cfccd96519aab9f8d
ahreum <117738875+aahreum@users.noreply.github.com>
2025-10-02 22:55:14 +0900

    🐛 Fix: 로딩 카드 개수 수정

--- src/constants/rollingPaperList.js
+++ src/constants/rollingPaperList.js
@@ -1,8 +1,7 @@
 /**
  * 초기 로딩 시 MessageList에 표시할 그리드 슬롯의 총 개수입니다.
- * (메시지 추가 버튼 슬롯 1개 + 초기 데이터 메시지 슬롯 8개 = 9)
  */
-const MESSAGE_LIST_SKELETON_COUNT = 9;
+const MESSAGE_LIST_SKELETON_COUNT = 3;
 
 export const MESSAGE_LIST_SKELETON_ARRAY = Array.from({
   length: MESSAGE_LIST_SKELETON_COUNT,
