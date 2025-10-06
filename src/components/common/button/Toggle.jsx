742892ec24202d55d51805da543ba5f08050ea20
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-10-06 16:08:44 +0900

    ✨ Feat: 재미나이 리뷰반영 1차 수정

--- src/components/common/button/Toggle.jsx
+++ src/components/common/button/Toggle.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '@/components/common/button/Button';
+import { cn } from '@/utils/style';
 
 const ToggleSwitch = () => {
   const [isEditMode, setIsEditMode] = useState(false);
@@ -9,27 +10,22 @@ const ToggleSwitch = () => {
   };
 
   return (
-    <>
-      {isEditMode ? (
-        <Button
-          onClick={handleClick}
-          className="relative flex h-[30px] w-[70px] items-center justify-end gap-0.5 overflow-hidden rounded-full bg-[#181818cc] p-0.5">
-          <div className="relative aspect-[1] h-[26px] w-[26px] rounded-full bg-white" />
-          <div className="absolute left-2.5 top-[calc(50%-10px)] flex h-5 items-center justify-center whitespace-nowrap text-sm font-bold leading-5 text-white">
-            편집
-          </div>
-        </Button>
-      ) : (
-        <Button
-          onClick={handleClick}
-          className="relative flex h-[30px] w-[70px] scale-x-[-1] items-center justify-end gap-0.5 overflow-hidden rounded-full bg-neutral-200 p-0.5">
-          <div className="relative aspect-[1] h-[26px] w-[26px] rounded-full bg-white" />
-          <div className="absolute left-2.5 top-[calc(50%-10px)] flex h-5 scale-x-[-1] items-center justify-center whitespace-nowrap text-sm font-bold leading-5 text-[#999999]">
-            기본
-          </div>
-        </Button>
-      )}
-    </>
+    <Button
+      onClick={handleClick}
+      theme={null}
+      className={cn(
+        'relative flex h-[30px] w-[70px] items-center justify-end gap-0.5 overflow-hidden rounded-full p-0.5',
+        isEditMode ? 'bg-[#181818cc]' : 'scale-x-[-1] bg-neutral-200'
+      )}>
+      <div className="relative aspect-[1] h-[26px] w-[26px] rounded-full bg-white" />
+      <div
+        className={cn(
+          'absolute left-2.5 top-[calc(50%-10px)] flex h-5 items-center justify-center whitespace-nowrap text-sm font-bold leading-5',
+          isEditMode ? 'text-white' : 'scale-x-[-1] text-[#999999]'
+        )}>
+        {isEditMode ? '편집' : '기본'}
+      </div>
+    </Button>
   );
 };
 
