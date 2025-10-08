36b9375a58a5a4f95302a431badbe37c490e6669
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-08 17:24:08 +0900

    ✨ Feat: DOMPurify 공통 설정 파일(SANITIZE_CONFIG) 추가

---
+++ src/utils/sanitizeConfig.js
@@ -0,0 +1,31 @@
+const SANITIZE_CONFIG = {
+  ALLOWED_TAGS: [
+    'p',
+    'br',
+    'strong',
+    'b',
+    'em',
+    'i',
+    'u',
+    's',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'ul',
+    'ol',
+    'li',
+    'a',
+    'img',
+    'span',
+    'div',
+    'blockquote',
+    'code',
+    'pre',
+  ],
+  ALLOWED_ATTR: ['href', 'src', 'alt', 'title', 'class', 'style'],
+};
+
+export default SANITIZE_CONFIG;
