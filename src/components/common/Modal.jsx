ba472c3bb5f16d420db9da4e9a3c004e68d5804e
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-08 15:32:32 +0900

    ✨ Feat: 공용 모달 컴포넌트 Modal.jsx 추가

---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,54 @@
+import '@/style/base/modal.css';
+import Button from '@/components/common/button/Button';
+import ModalHeader from '@/components/common/ModalHeader';
+import PostContent from '@/components/common/PostContent';
+import ModalPortal from './ModalPortal';
+
+const footerStyle = 'pt-6 border-t border-gray-200 flex justify-center';
+const contentBodyStyle = 'flex-grow overflow-y-auto px-4 py-2';
+
+function Modal({
+  isOpen,
+  onClose,
+  contentHtml,
+  sender,
+  role,
+  profileImgUrl,
+  createdAt,
+  font,
+}) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <ModalPortal>
+      <div className="modal-backdrop" onClick={onClose} aria-hidden="true" />
+
+      <div className="modal-container" role="dialog" aria-modal="true">
+        <ModalHeader
+          sender={sender}
+          role={role}
+          date={createdAt}
+          profileImgUrl={profileImgUrl}
+        />
+
+        <div className={contentBodyStyle}>
+          <PostContent htmlContent={contentHtml} font={font} />
+        </div>
+
+        <div className={footerStyle}>
+          <Button
+            onClick={onClose}
+            theme="primary"
+            size={56}
+            className="w-full max-w-[150px]">
+            확인
+          </Button>
+        </div>
+      </div>
+    </ModalPortal>
+  );
+}
+
+export default Modal;
