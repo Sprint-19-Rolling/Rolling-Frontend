0c67e6b09f5d64cdc9e85ac95092909b91b3f7bc
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-09 21:59:54 +0900

    ♻️ Refactor: PostDetail 컴포넌트에서 중복된 Header/Footer 제거

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,6 +1,4 @@
 import { Link, useParams } from 'react-router';
-import Footer from '@/components/common/Footer';
-import Header from '@/components/common/Header';
 import MessageList from '@/components/rolling-paper-list/MessageList';
 
 const PostDetail = () => {
@@ -8,8 +6,6 @@ const PostDetail = () => {
 
   return (
     <>
-      <Header />
-      <SubHeader recipientId={id} />
       {/* TODO: !아래 main 태그 배경 컴포넌트로 변경 필요! */}
       <main className="bg-beige-200 wrapper-px min-h-[calc(100dvh-297px)] w-full">
         <div className="content pt-15 pb-25">
@@ -18,7 +14,6 @@ const PostDetail = () => {
           <MessageList recipientId={id} />
         </div>
       </main>
-      <Footer />
     </>
   );
 };
