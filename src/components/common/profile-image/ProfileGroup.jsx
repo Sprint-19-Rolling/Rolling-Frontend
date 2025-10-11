fbce806cc2a386bf95bee67145e36b62a4b6bdfc
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-11 13:52:33 +0900

    🐛 Fix: ProfileGroup 컴포넌트 recentMessages 기본값([]) 추가로 undefined 오류 방지

--- src/components/common/profile-image/ProfileGroup.jsx
+++ src/components/common/profile-image/ProfileGroup.jsx
@@ -11,11 +11,13 @@ import ProfileImage from './ProfileImage';
  * 숨겨진 참가자 수를 표시하는 컴포넌트입니다.
  * @component
  * @param {object} props
- * @param {Array<MessageParticipant>} props.recentMessages - 화면에 표시할 최근 메시지 작성자 목록 (최대 3개)
+ * @param {Array<MessageParticipant>} [props.recentMessages=[]] - 화면에 표시할 최근 메시지 작성자 목록 (최대 3개). 기본값은 빈 배열.
  * @param {number} props.messageCount - 롤링페이퍼에 작성된 전체 메시지 개수
  * @returns {JSX.Element} Profile Group 컴포넌트
  */
-const ProfileGroup = ({ recentMessages, messageCount }) => {
+// recentMessages에 기본값 []을 설정하여 undefined일 경우의 오류를 방지합니다.
+const ProfileGroup = ({ recentMessages = [], messageCount }) => {
+  // recentMessages가 빈 배열이거나 유효한 배열이므로 안전하게 .length 접근 가능
   const hiddenCount = messageCount - recentMessages.length;
 
   return (
