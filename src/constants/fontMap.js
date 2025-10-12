9fd8251b72eb752c8109f819315a740054524f16
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-12 19:28:04 +0900

    🎉 Init: 글꼴 매핑 상수 파일 생성

---
+++ src/constants/fontMap.js
@@ -0,0 +1,14 @@
+export const fontMap = {
+  'noto-sans': "'Noto Sans', sans-serif",
+  pretendard: "'Pretendard', sans-serif",
+  'nanum-myeongjo': "'Nanum Myeongjo', serif",
+  handletter: "'Nanum Pen Script', cursive",
+};
+
+// 드롭다운에 표시될 한글/영어 라벨
+export const fontDisplayNames = {
+  'noto-sans': 'Noto Sans',
+  pretendard: 'Pretendard',
+  'nanum-myeongjo': '나눔명조',
+  handletter: '나눔손글씨 손편지체',
+};
