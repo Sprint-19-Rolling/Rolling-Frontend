5444d0676930b5a8fbf99b12e4f1d799cf26ed7d
ssml0905 <growtopiaolk@naver.com>
2025-10-12 15:35:56 +0900

    ✨ Feat: 리스트 페이지

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,18 +1,21 @@
 import LinkButton from '@/components/common/button/LinkButton';
 import FloatingButtonContainer from '@/components/common/FloatingButtonContainer';
 import Title from '@/components/common/Title';
-import RollingPaperList from '@/components/papers/RollingPaperList';
+import LikeSortedRollingPaper from '@/components/papers/LikeSortedRollingPaper';
+import RecentSortedRollingPaper from '@/components/papers/RecentSortedRollingPaper';
+import { cn } from '@/utils/style';
 
 const List = () => {
+  const listStyle = 'flex flex-col gap-4';
   return (
-    <div className="py-15 flex flex-col gap-12">
-      <div>
+    <div className="py-15 flex flex-col gap-6">
+      <div className={listStyle}>
         <Title>인기 롤링 페이퍼 🔥</Title>
-        {/* TODO: 롤링페이퍼 인기순 리스트 적용하기 */}
+        <LikeSortedRollingPaper />
       </div>
-      <div>
+      <div className={cn(listStyle, 'mt-6')}>
         <Title>최근에 만든 롤링 페이퍼 ⭐️️</Title>
-        <RollingPaperList />
+        <RecentSortedRollingPaper />
       </div>
       <FloatingButtonContainer>
         <LinkButton to={'/post'} size={56} full="mobile">
