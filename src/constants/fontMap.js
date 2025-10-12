4c7e0d922d7eb4e30e984dbef3fb7dd48dcfc414
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-12 21:17:47 +0900

    ✨ Feat: fontMap과 fontDisplayNames에 한글 폰트 이름 추가

--- src/constants/fontMap.js
+++ src/constants/fontMap.js
@@ -1,8 +1,8 @@
 export const fontMap = {
   'noto-sans': 'Noto Sans',
   pretendard: 'Pretendard',
-  'nanum-myeongjo': 'Nanum Myeongjo',
-  handletter: 'Nanum Pen Script',
+  'nanum-myeongjo': '나눔명조',
+  handletter: '나눔손글씨 손편지체',
 };
 
 // 드롭다운에 표시될 한글/영어 라벨
