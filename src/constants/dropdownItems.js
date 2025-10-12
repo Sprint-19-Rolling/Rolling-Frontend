956883a38cd0059772934330d720e1d432ee2ff0
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-12 19:27:40 +0900

    🎉 Init: 드롭다운용 상수 파일 생성

---
+++ src/constants/dropdownItems.js
@@ -0,0 +1,12 @@
+export const RELATIONSHIP_ITEMS = ['지인', '친구', '가족', '동료'];
+
+export const FONT_ITEMS = [
+  'Noto Sans',
+  'Pretendard',
+  '나눔명조',
+  '나눔손글씨 손편지체',
+];
+
+// 필요 시 다른 드롭다운용 아이템도 여기에 추가 가능
+// export const CATEGORY_ITEMS = [...];
+// export const COLOR_THEME_ITEMS = [...];
