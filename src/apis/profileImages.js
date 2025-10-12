28793042970a1c533b98e8f9755f44cdaf0f6957
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-13 01:29:24 +0900

    🐛 Fix: 프로필 이미지 API 오류 처리 개선 - 네트워크 에러 시 예외 처리 방식 수정 - 불필요한 try-catch 제거하고 간결한 코드로 리팩토링

--- src/apis/profileImages.js
+++ src/apis/profileImages.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { api } from '@/apis/axios';
 
 /**
@@ -6,17 +5,6 @@ import { api } from '@/apis/axios';
  * @returns {Promise<string[]>} 프로필 이미지 URL 배열
  */
 export const fetchProfileImages = async (signal) => {
-  try {
-    const response = await api.get('profile-images/', { signal });
-    return response.data.imageUrls || [];
-  } catch (err) {
-    // CanceledError는 조용히 무시 (React Strict Mode에서 정상 동작)
-    if (axios.isCancel(err) || err.name === 'CanceledError') {
-      return [];
-    }
-
-    // 실제 네트워크 에러만 로깅
-    console.error('❌ 프로필 이미지 불러오기 실패:', err);
-    throw err; // 에러를 다시 던져서 useDataFetch에서 처리하도록
-  }
+  const response = await api.get('profile-images/', { signal });
+  return response.data.imageUrls || []; // 이미지 URL 배열이 없으면 빈 배열 반환
 };
