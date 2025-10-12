1bb94d046771f78923416f5247d1a5f1eb660b18
Choi hee ruk <chlgmlfkr593@naver.com>
2025-10-12 19:30:29 +0900

    ♻️ Refactor: useProfileImages 훅을 useDataFetch 기반으로 리팩토링

--- src/hooks/useProfileImages.js
+++ src/hooks/useProfileImages.js
@@ -1,26 +1,33 @@
 import { useState, useEffect } from 'react';
-import { fetchProfileImages } from '@/apis/messageApi';
+import { fetchProfileImages } from '@/apis/messages';
+import { useDataFetch } from '@/hooks/useDataFetch';
 
 /**
  * 프로필 이미지 목록을 관리하는 커스텀 훅
- * @returns {Object} profileImages, profileImageURL, setProfileImageURL
+ * @returns {Object} profileImages, profileImageURL, setProfileImageURL, loading, error
  */
 export const useProfileImages = () => {
-  const [profileImages, setProfileImages] = useState([]);
+  const {
+    data: profileImages = [],
+    loading,
+    error,
+    refetch,
+  } = useDataFetch(fetchProfileImages);
+
   const [profileImageURL, setProfileImageURL] = useState('');
 
   useEffect(() => {
-    const loadProfileImages = async () => {
-      const urls = await fetchProfileImages();
-      setProfileImages(urls);
-      setProfileImageURL(urls[0]);
-    };
-    loadProfileImages();
-  }, []);
+    if (profileImages.length > 0) {
+      setProfileImageURL(profileImages[0]);
+    }
+  }, [profileImages]);
 
   return {
     profileImages,
     profileImageURL,
     setProfileImageURL,
+    loading,
+    error,
+    refetch,
   };
 };
